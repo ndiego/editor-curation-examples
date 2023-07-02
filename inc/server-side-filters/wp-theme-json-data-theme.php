b5ec55731f1a1dc6f55094a9fb23bca91951522a
Nick Diego <nickmdiego@gmail.com>
2023-07-01 20:48:19 -0500

    Add custom variable example.

--- inc/server-side-filters/wp-theme-json-data-theme.php
+++ inc/server-side-filters/wp-theme-json-data-theme.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This function modifies the theme JSON data by updating the theme's color
- * palette based on the user's logged in status.
+ * palette based on the user's login status.
  *
  * @param object $theme_json The original theme JSON data.
  * @return object The modified theme JSON data.
@@ -40,6 +40,9 @@ function ece_modify_color_palette_if_logged_in( $theme_json ) {
 						),
 					),
 				),
+				'custom' => array(
+					'myCustomVar' => 'this-is-custom',
+				),
 			),
 			'styles'  => array(
 				'elements' => array(
@@ -61,12 +64,39 @@ function ece_modify_color_palette_if_logged_in( $theme_json ) {
 	return $theme_json;
 }
 
+/**
+ * This function modifies the theme JSON data by creating a custom
+ * CSS variables for various breakpoints.
+ *
+ * @param object $theme_json The original theme JSON data.
+ * @return object The modified theme JSON data.
+ */
+function ece_create_breakpoint_variables( $theme_json ) {
+
+	$new_data = array(
+		'version'  => 2,
+		'settings' => array(
+			'custom' => array(
+				'breakpoint' => array(
+					'small'  => '360px',
+					'medium' => '780px',
+					'large'  => '1080px',
+				),
+			),
+		),
+	);
+
+	// Return the modified theme JSON data.
+	return $theme_json->update_with( $new_data );
+}
+
 // For the filter to work properly, it must be run after theme setup.
 function ece_apply_theme_json_theme_filters() {
 
 	// Check to make sure the theme has a theme.json file.
 	if ( wp_theme_has_theme_json() ) {
 		add_filter( 'wp_theme_json_data_theme', 'ece_modify_color_palette_if_logged_in' );
+		add_filter( 'wp_theme_json_data_theme', 'ece_create_breakpoint_variables' );
 	}
 }
 add_action( 'after_setup_theme', 'ece_apply_theme_json_theme_filters' );
