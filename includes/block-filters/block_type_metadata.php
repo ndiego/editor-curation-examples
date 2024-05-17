c7d09d1a0f02bc41fa2ca4c14b7206633912b852
Nick Diego <nickmdiego@gmail.com>
2024-05-17 11:42:25 -0500

    Add a few more examples.

--- includes/block-filters/block_type_metadata.php
+++ includes/block-filters/block_type_metadata.php
@@ -2,9 +2,6 @@
 /**
  * Disable background color and gradient support for Heading blocks.
  *
- * This function hooks into the 'block_type_metadata' filter to modify the block
- * settings for Heading blocks, disabling support for background color and gradients.
- *
  * @param array $metadata The block type metadata.
  * @return array          The modified block type metadata.
  */
@@ -28,12 +25,74 @@ function ece_disable_heading_background_color_and_gradients( $metadata ) {
 add_filter( 'block_type_metadata', 'ece_disable_heading_background_color_and_gradients' );
 
 /**
+ * Add a border support to a group of allowed blocks.
+ *
+ * @param array $metadata The block type metadata.
+ * @return array          The modified block type metadata.
+ */
+function ece_add_border_support_to_allowed_blocks( $metadata ) {
+
+    $allowed_blocks = array( 
+        'core/heading',
+        'core/paragraph',
+        'core/column',
+    );
+
+    // Only apply the filter to allowed blocks.
+    if ( ! isset( $metadata['name'] ) || ! in_array( $metadata['name'], $allowed_blocks ) ) {
+        return $metadata;
+    }
+
+    // Add border support.
+    $metadata['supports']['__experimentalBorder'] = array(
+        'color'  => true,
+        'style'  => true,
+        'width'  => true,
+        'radius' => true,
+    );
+
+    // Set default controls for border.
+    $metadata['supports']['__experimentalBorder']['__experimentalDefaultControls'] = array(
+        'color'  => false,
+        'style'  => false,
+        'width'  => false,
+        'radius' => true,
+    );
+
+    return $metadata;
+}
+add_filter( 'block_type_metadata', 'ece_add_border_support_to_allowed_blocks' );
+
+/**
+ * Disable default spacing controls for File blocks.
+ *
+ * @param array $metadata The block type metadata.
+ * @return array          The modified block type metadata.
+ */
+function ece_disable_spacing_default_controls_for_file_blocks( $metadata ) {
+
+    // Only apply the filter to File blocks.
+    if ( ! isset( $metadata['name'] ) || 'core/file' !== $metadata['name'] ) {
+        return $metadata;
+    }
+    
+    // Disable default controls for spacing.
+    $metadata['supports']['spacing']['__experimentalDefaultControls'] = array(
+        'margin'  => false,
+        'padding' => false,
+    );
+
+    return $metadata;
+}
+add_filter( 'block_type_metadata', 'ece_disable_spacing_default_controls_for_file_blocks' );
+
+/**
  * Add a group of allowed blocks to the new custom Featured category.
  *
  * @param array $metadata The block type metadata.
  * @return array          The modified block type metadata.
  */
-function ece_add_heading_to_featured_category( $metadata ) {
+function ece_add_allowed_blocks_to_featured_category( $metadata ) {
 
     $allowed_blocks = array( 
         'core/heading',
@@ -54,7 +113,7 @@ function ece_add_heading_to_featured_category( $metadata ) {
         array( 'category' => 'featured' )
     );
 }
-add_filter( 'block_type_metadata', 'ece_add_heading_to_featured_category' );
+add_filter( 'block_type_metadata', 'ece_add_allowed_blocks_to_featured_category' );
 
 /**
  * Adds a new custom block category at the beginning of the block categories array.
