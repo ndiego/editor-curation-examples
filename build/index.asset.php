c84a00ff83fc1cf8b4af630bda16b00e41a9c86c
Nick Diego <nickmdiego@gmail.com>
2024-07-02 10:13:14 -0500

    Fresh build.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-data', 'wp-hooks'), 'version' => '693bc9d485c33d04a5a4');
+<?php return array('dependencies' => array('wp-data', 'wp-hooks'), 'version' => 'e90262230a18b97a8f01');
